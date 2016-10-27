f574ec7e29e91bffbd3dfe8644f2c32510cf092e
Amir Hameed <picwellwisher12pk@gmail.com>
2016-10-27 14:33:50 +0500

    Tabs count in Icon badge is not dynamic , changes on Tab create , remove , attach or detach.

--- js/background.js
+++ js/background.js
@@ -1,9 +1,17 @@
-chrome.tabs.query({
-    currentWindow: true
-}, function(tabs) {
-	chrome.browserAction.setBadgeText({
-	text : String(tabs.length)
-	}); 
-});
+function setTabCountInBadge(dataAndEvents,deepDataAndEvents){
+	chrome.tabs.query({
+	    currentWindow: true
+	}, function(tabs) {
+		chrome.browserAction.setBadgeText({
+		text : String(tabs.length)
+		}); 
+	});
+}
+chrome.runtime.onInstalled.addListener(setTabCountInBadge)
+chrome.tabs.onCreated.addListener(setTabCountInBadge);
+chrome.tabs.onRemoved.addListener(setTabCountInBadge);
+chrome.tabs.onMoved.addListener(setTabCountInBadge);
+chrome.tabs.onDetached.addListener(setTabCountInBadge);
+chrome.tabs.onAttached.addListener(setTabCountInBadge);
 
 
