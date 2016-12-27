d3d411cbcf18c876a892e6452b4e808f1b29b1cb
Amir Hameed <picwellwisher12pk@gmail.com>
2016-12-28 03:09:43 +0500

    info modal featuer added.

--- src/scripts/activetabs.tsx
+++ src/scripts/activetabs.tsx
@@ -1,3 +1,5 @@
+                    // glyphicon glyphicon-option-vertical
+                    // glyphicon glyphicon-menu-down
 class ActiveTabs extends React.Component {
   constructor(props) {
     super(props);
@@ -19,7 +21,7 @@ class ActiveTabs extends React.Component {
         return (<ul className="tabs-list list-group">
                 {activeTabs.map(function(value) {
                     // console.log("rendering li",value.title);
-                    return <Tab key={value.id} id={value.id} pinned={value.pinned} audible={value.audible} position={value.index} url={value.url} title={value.title} favIconUrl={value.favIconUrl} status={value.status}/>
+                    return <Tab key={value.id} id={value.id} pinned={value.pinned} audible={value.audible} position={value.index} url={value.url} title={value.title} favIconUrl={value.favIconUrl} status={value.status} data={value}/>
                 })}
 
             </ul>
@@ -32,6 +34,8 @@ ActiveTabs.propTypes = {
 ActiveTabs.defaultProps= {
         data: []
 };
+
+
 class Tab extends React.Component{
    constructor(props) {
     super(props);
@@ -42,11 +46,11 @@ class Tab extends React.Component{
         position: this.props.position,
         favicon: this.props.favIconUrl,
         audible: this.props.audible,
-        status: this.props.status
+        status: this.props.status,
+        data: this.props.data
         }; 
     }
     focusTab(id){
-
         console.log("inside open",id);
         packageAndBroadcast(sender,"background","focusTab",id);
     }
@@ -66,6 +70,13 @@ class Tab extends React.Component{
         if(confirm(`Are you sure you want to close the following tab\n`+(title)))
             {packageAndBroadcast(sender,"background","closeTab",id);}
     }
+    infoModal(dataV){
+        console.log("trigger infoModal",this,dataV);
+        InfoModal.setState({
+            data:dataV 
+        });
+        jQuery('#infoModal').modal('show');
+    }
     render(){
         
         
@@ -76,8 +87,9 @@ class Tab extends React.Component{
                 <span className = {`clickable glyphicon glyphicon-volume-off audible`+(_this.state.audible ? ` `: ` disabled`)} onClick={_this.muteTab.bind(_this,_this.props.id,_this.state.audible)} aria-hidden='true'></span>
                 <img src={_this.state.favicon}/>
                 <strong className="clickable" title={_this.state.title} onClick={_this.focusTab.bind(null,_this.props.id)}>{_this.state.title}</strong>
-                <span>{_this.state.url}</span>
                 <div className="options pull-right">
+
+                    <span className="glyphicon glyphicon-option-vertical" onClick={_this.infoModal.bind(null,_this.state.data)} ></span>
                     <span data-id={_this.props.id} onClick={_this.closeTab.bind(null,_this.props.id,_this.state.title)} data-command='remove' className='clickable remove-tab glyphicon glyphicon-remove' aria-hidden='true'></span>
                 </div>
            </li>
@@ -85,6 +97,64 @@ class Tab extends React.Component{
         
     }
 }
+class InfoModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        data: this.props.data
+        }; 
+   }
+
+    handleClick() {
+        console.log(this); // null
+    }
+    
+    render() {
+        let _this = this;
+        let data = this.state.data;
+        // console.group("Rendering");
+        console.log("rendering infoModal",data.length,"items",data);
+        
+        return (
+                    <div className="modal-dialog modal-lg" role="document">
+                        <div className="modal-content">
+                           <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 className="modal-title" id="myModalLabel">
+                                <img src={_this.state.data.favIconUrl}/> 
+                                {this.state.data.title}
+                            </h4>
+                          </div>
+                          <div className="modal-body">
+                           <p><strong>ID :</strong> {this.state.data.id}</p>
+                           <p><strong>Index(position) :</strong> {this.state.data.index}</p>
+                           <p><strong>URL :</strong> {this.state.data.url}</p>
+                           <p><strong>Title :</strong> {this.state.data.title}</p>
+                           <p><strong>Status :</strong> {this.state.data.status}</p>
+                           <p><strong>Pinned :</strong> {this.state.data.pinned ? "Yes" : "No"}</p>
+                           <p><strong>Audible :</strong> {this.state.data.audible ? "Yes" : "No"}</p>
+                           <p><strong>Active :</strong> {this.state.data.active ? "Yes" : "No"}</p>
+                           <p><strong>Selected :</strong> {this.state.data.selected ? "Yes" : "No"}</p>
+                           <p><strong>Width :</strong> {this.state.data.width}</p>
+                           <p><strong>Height :</strong> {this.state.data.height}</p>
+                           <p><strong>WindowID :</strong> {this.state.data.windowId}</p>
+                          </div>
+                          <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                          </div>
+                        </div>
+                      </div>
+            )
+    }
+
+  }
+
+InfoModal.propTypes = {
+ data: React.PropTypes.array.isRequired
+};
+InfoModal.defaultProps= {
+        data: []
+};
 
 
     let Note = React.createClass({
