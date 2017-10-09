12d2468c61ff43d30d3ca57d962db9e7b7acd5ad
Amir Hameed <picwellwisher12pk@gmail.com>
2017-10-10 01:36:21 +0500

    updated communcation file package is replaced with packed due to some reserved word conflict. and Type specifications from Typescript has been removed.

---
+++ src/scripts/components/communications.js
@@ -0,0 +1,13 @@
+  function packagedData(sender, receiver, targetMethod, data) {
+      let pack = {
+          sender: sender,
+          receiver: receiver,
+          targetMethod: targetMethod,
+          data: data
+      };
+      return pack;
+  }
+
+  export default function packagedAndBroadcast(sender = sender, receiver, targetMethod, data) {
+      chrome.runtime.sendMessage(packagedData(sender, receiver, targetMethod, data));
+  }
