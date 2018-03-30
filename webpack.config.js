ddf1386eb58b66757205ac1adb270a6ed18f6272
Amir Hameed <picwellwisher12pk@gmail.com>
2018-03-30 19:47:03 +0500

    tried few module update changes. Production and Development modes working fine.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,38 @@
-var webpack = require("webpack"),
+let webpack = require("webpack"),
     path = require("path"),
     fileSystem = require("fs"),
     env = require("./utils/env"),
-    webpackValidator = require('webpack-validator'),
+    // CleanWebpackPlugin = require("clean-webpack-plugin"),
+    // CopyWebpackPlugin = require("copy-webpack-plugin"),
     HtmlWebpackPlugin = require("html-webpack-plugin"),
-    ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    WriteFilePlugin = require("write-file-webpack-plugin"),
+    WebpackBar = require("webpackbar");
 
 // load the secrets
-var alias = {};
+alias = {};
 
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+let secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
 
-var images = ["jpg", "jpeg", "png", "gif", "svg"];
-var fonts = ["eot", "otf", "ttf", "woff", "woff2"];
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
+let images = ["jpg", "jpeg", "png", "gif", "svg"];
+let fonts = ["eot", "otf", "ttf", "woff", "woff2"];
+let fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
 
 if (fileSystem.existsSync(secretsPath)) {
     alias["secrets"] = secretsPath;
 }
 
-var options = {
+let options = {
     context: __dirname,
     entry: {
-        tabs: path.join(__dirname, "src", "scripts", "app.jsx"),
-        options: path.join(__dirname, "src", "scripts", "components", "options.jsx"),
+        tabs: path.join(__dirname, "src", "scripts", "tabs.jsx"),
+        options: path.join(
+            __dirname,
+            "src",
+            "scripts",
+            "components",
+            "options.jsx"
+        ),
         sessions: path.join(__dirname, "src", "scripts", "sessions.jsx"),
         background: path.join(__dirname, "src", "scripts", "background.jsx")
     },
@@ -33,27 +41,18 @@ var options = {
         filename: "js/[name].js"
     },
     module: {
-        rules: [
-            // {
-            //     test: /\.css$/,
-            //     loader: "file-loader!css-loader",
-            //     options: {
-            //         outputPath: "css/"
-            //     },
-            //     exclude: /node_modules/
-            // },
-            {
+        rules: [{
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
+                loader: ExtractTextPlugin.extract("css-loader!sass-loader"),
                 exclude: /node_modules/
             },
             {
-                test: new RegExp('\.(' + images.join('|') + ')$'),
+                test: new RegExp(".(" + images.join("|") + ")$"),
                 loader: "file-loader?name=images/[name].[ext]",
                 exclude: /node_modules/
             },
             {
-                test: new RegExp('\.(' + fonts.join('|') + ')$'),
+                test: new RegExp(".(" + fonts.join("|") + ")$"),
                 loader: "file-loader?name=/fonts/[name].[ext]",
                 exclude: /node_modules/
             },
@@ -63,18 +62,36 @@ var options = {
                 exclude: /node_modules/
             },
             {
-                test: /\.(js|jsx)$/,
+                test: /\.js$/,
                 loader: "babel-loader",
                 exclude: /node_modules/
+            },
+            {
+                test: /\.jsx$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: [
+                        "stage-3",
+                        "env",
+                        "react"
+                    ]
+                }
             }
         ]
     },
     resolve: {
         alias: alias,
-        extensions: fileExtensions.map(extension => ("." + extension)).concat([".jsx", ".js", ".css"]),
-        modules: [path.resolve(__dirname, 'src'), 'node_modules', path.resolve(__dirname, 'build')],
-        descriptionFiles: ['package.json'],
-        moduleExtensions: ['-loader'],
+        extensions: fileExtensions
+            .map(extension => "." + extension)
+            .concat([".jsx", ".js", ".css"]),
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "src"),
+            path.resolve(__dirname, "build")
+        ],
+        descriptionFiles: ["package.json"],
+        moduleExtensions: ["-loader"]
     },
 
     plugins: [
@@ -87,15 +104,12 @@ var options = {
         }),
         // expose and write the allowed env vars on the compiled bundle
         new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+            "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
+            CLIENT: process.env.CLIENT || "browser"
         }),
-        // new HtmlWebpackPlugin({
-        //     template: path.join(__dirname, "src", "tabs.html"),
-        //     filename: "tabs.html",
-        //     chunks: ["tabs"]
-        // }),
 
         new HtmlWebpackPlugin({
+            title: "Excited Gem | Tabs",
             template: path.join(__dirname, "src", "tabs.html"),
             filename: "tabs.html",
             chunks: ["tabs"]
@@ -111,19 +125,15 @@ var options = {
             filename: "sessions.html",
             chunks: ["sessions"]
         }),
-        // new HtmlWebpackPlugin({
-        //     template: path.join(__dirname, "src", "background.html"),
-        //     filename: "background.html",
-        //     chunks: ["background"]
-        // }),
-
-        new WriteFilePlugin()
+        new WriteFilePlugin(),
+        new WebpackBar({ profile: true })
     ]
 };
 
-// if (env.NODE_ENV === "development") {
-//     options.devtool = "cheap-module-eval-source-map";
-// }
+
+if (env.NODE_ENV === "development") {
+    options.devtool = "cheap-module-eval-source-map";
+}
 options.devtool = "cheap-source-map";
 
 module.exports = options;
