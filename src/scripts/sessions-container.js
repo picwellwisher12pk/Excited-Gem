2926f4f17d76cf68c297c1fc5776be3d10b0ce73
Amir Hameed <picwellwisher12pk@gmail.com>
2018-04-12 17:21:09 +0500

    removed unused dependencies

--- src/scripts/sessions-container.js
+++ src/scripts/sessions-container.js
@@ -1,17 +1,13 @@
 //Vendors
 const $ = (jQuery = require('jquery'));
-// import "./vendor/jquery.min.js";/*?*/
 const bootstrap = require('bootstrap');
-// const fontawesome5Light = require ( "./vendor/fontawesome5/fa-light.min");
-// const fontawesome5Solid = require ( "./vendor/fontawesome5/fa-solid.min");
-// const fontawesome5all = require ( "./vendor/fontawesome5/fontawesome-all.min");
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import * as general from './components/general.js';
 import Sessions from './react-components/session.js';
 import packagedAndBroadcast from './components/communications.js';
 import { saveSessions, getSessions } from './components/getsetSessions.js';
+
 //Styles
 import '../styles/bootstrap.scss';
 import '../styles/fontawesome5.scss';
