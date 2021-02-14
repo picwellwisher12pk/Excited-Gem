2df119c9540b5567392b73ecc323ed04d92693d3
Amir Hameed <picwellwisher12pk@gmail.com>
2021-02-14 05:52:59 +0500

    Tab.js refactored: converted functional component Image now will only show alt and title when it has src url available.

--- src/scripts/components/Accordion/TabsGroup/Tab/Tab.js
+++ src/scripts/components/Accordion/TabsGroup/Tab/Tab.js
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import {FontAwesomeIcon as FA} from '@fortawesome/react-fontawesome';
 import {faVolume} from '@fortawesome/pro-solid-svg-icons/faVolume';
 import {faVolumeOff} from '@fortawesome/pro-light-svg-icons/faVolumeOff';
@@ -11,102 +11,79 @@ import {connect} from 'react-redux';
 import ACTIONS from '../../../../modules/action';
 
 let browser = require('webextension-polyfill');
-
-class Tab extends PureComponent {
-  // componentWillReceiveProps(props) {
-  //   this.setState({url: props.url, title: props.title, favicon: props.favIconUrl});
-  //
-  //   //Tabs are either ActiveTabs that is tabs representing on browser
-  //   // Or Links/Tabs on session stored
-  //
-  //   if (props.activeTab) {
-  //     this.setState({ id: props.id });
-  //     if (props.key) this.setState({ key: props.key });
-  //     this.setState({
-  //       indexkey: props.indexkey,
-  //       discarded: props.discarded,
-  //       pinned: props.pinned,
-  //       position: props.position,
-  //       audible: props.audible,
-  //       muted: props.muted,
-  //       checked: props.checked,
-  //       status: props.status,
-  //     });
-  //   }
-  // }
-  render() {
-    let {title, url, checked, pinned, discarded} = this.props;
-    //Search Highlighting;
-    if (this.props.searchTerm) {
-      const {searchTerm} = this.props;
-      let regex;
-      try {
-        regex = new RegExp(searchTerm, 'gi');
-      } catch (e) {
-        console.log("Bad Regular Expressions:", e, searchTerm);
-      }
-      title = this.props.title.replace(regex, `<mark>${searchTerm}</mark>`);
-      url = this.props.url.replace(regex, `<mark>${searchTerm}</mark>`);
+const Tab = (props) => {
+  let {title, url, checked, pinned, discarded} = props;
+  //Search Highlighting;
+  if (props.searchTerm) {
+    const {searchTerm} = props;
+    let regex;
+    try {
+      regex = new RegExp(searchTerm, 'gi');
+    } catch (e) {
+      console.error("Bad Regular Expressions:", e, searchTerm);
     }
+    title = props.title.replace(regex, `<mark>${searchTerm}</mark>`);
+    url = props.url.replace(regex, `<mark>${searchTerm}</mark>`);
+  }
 
-    let loading = this.props.status === 'loading', audible = this.props.audible || this.props.muted, linkProps = null,
-      actionButtons = null, audioIcon = '';
-    if (!audible) audioIcon = <FA icon={faVolumeOff} className={'text-info'} fixedWidth/>;
-    if (audible && !this.props.muted) audioIcon = <FA icon={faVolume} className={'text-info'} fixedWidth/>;
-    if (audible && this.props.mutedInfo.muted)
-      audioIcon = <FA icon={faVolumeSlash} className={'text-info'} fixedWidth/>;
-    let iconPinned = <FA icon={pinned ? fasThumbtack : faThumbtack} className={'text-primary'} fixedWidth/>;
+  let loading = props.status === 'loading', audible = props.audible || props.muted,
+    actionButtons = null, audioIcon = '';
+  if (!audible) audioIcon = <FA icon={faVolumeOff} className={'text-info'} fixedWidth/>;
+  if (audible && !props.muted) audioIcon = <FA icon={faVolume} className={'text-info'} fixedWidth/>;
+  if (audible && props.mutedInfo.muted)
+    audioIcon = <FA icon={faVolumeSlash} className={'text-info'} fixedWidth/>;
+  let iconPinned = <FA icon={pinned ? fasThumbtack : faThumbtack} className={'text-primary'} fixedWidth/>;
 
-    //Markup of Action bar for active tabs: Right side buttons
-    if (this.props.activeTab) {
-      actionButtons = [
-        <li
-          key={1}
-          title="Un/Pin Tab"
-          className={`clickable pin-tab` + (pinned ? ` active` : ` disabled`)}
-          onClick={() => this.props.togglePin(this.props.id)}
-          aria-label="pinned"
-        >
-          {iconPinned}
-        </li>,
-        <li
-          key={2}
-          title="Un/Mute Tab"
-          className={`clickable sound-tab` + (audible ? ` active` : ` disabled`)}
-          onClick={() => this.props.toggleMute(this.props.id)}
-        >
-          {audioIcon}
-        </li>,
-        <li
-          key={3}
-          title="Close Tab"
-          className="clickable remove-tab"
-          data-id={this.props.id}
-          onClick={() => this.props.closeTab(this.props.id)}
-          data-command="remove"
-        >
-          <FA icon={faTimes} className={'text-danger'} />
-        </li>,
-      ];
+  //Markup of Action bar for active tabs: Right side buttons
+  if (props.activeTab) {
+    actionButtons = [
+      <li
+        key={1}
+        title="Un/Pin Tab"
+        className={`clickable pin-tab` + (pinned ? ` active` : ` disabled`)}
+        onClick={() => props.togglePin(props.id)}
+        aria-label="pinned"
+      >
+        {iconPinned}
+      </li>,
+      <li
+        key={2}
+        title="Un/Mute Tab"
+        className={`clickable sound-tab` + (audible ? ` active` : ` disabled`)}
+        onClick={() => props.toggleMute(props.id)}
+      >
+        {audioIcon}
+      </li>,
+      <li
+        key={3}
+        title="Close Tab"
+        className="clickable remove-tab"
+        data-id={props.id}
+        onClick={() => props.closeTab(props.id)}
+        data-command="remove"
+      >
+        <FA icon={faTimes} className={'text-danger'}/>
+      </li>,
+    ];
     } else {
       //Non-Active Tabs only get a remove button on action bar for now.
       actionButtons = (
         <li
           title="Remove"
           className="clickable remove-tab"
-          data-id={this.props.id}
-          onClick={() => this.props.removeTab(this.props.url)}
+          data-id={props.id}
+          onClick={() => props.removeTab(props.url)}
           data-command="remove"
         >
-          <FA icon={faTimes} />
+          <FA icon={faTimes}/>
         </li>
       );
     }
     return (
       <Draggable
-        draggableId={this.props.id + ""}
-        key={this.props.id}
-        index={this.props.index}
+        draggableId={props.id + ""}
+        key={props.id}
+        index={props.index}
         className={`tab-item ` + (checked && ` checked`) + (loading || discarded ? ` idle` : ` `)}
       >
         {provided => (
@@ -114,24 +91,24 @@ class Tab extends PureComponent {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
-            // id={`draggable-` + this.props.id}
-            key={this.props.id}
+            // id={`draggable-` + props.id}
+            key={props.id}
             className={`tab-item ` + (checked && ` checked`) + (loading || discarded ? ` idle` : ` `)}
           >
             <label className="tab-favicon" aria-label="favicon">
-              <img src={this.props.favIconUrl} alt={title}/>
+              <img src={props.favIconUrl} title={props.favIconUrl && title} alt={props.favIconUrl && title}/>
               <input
                 type="checkbox"
-                onChange={() => this.props.updateSelectedTabs(this.props.id, !this.props.checked)}
-                checked={this.props.checked}
+                onChange={() => props.updateSelectedTabs(props.id, !props.checked)}
+                checked={props.checked}
                 className="checkbox"
               />
             </label>
-            <a className="clickable tab-title clip" title={url} dangerouslySetInnerHTML={{ __html: title }} />
+            <a className="clickable tab-title clip" title={url} dangerouslySetInnerHTML={{__html: title}}/>
             <span
               className="tab-url trimmed dimmed clip"
-              dangerouslySetInnerHTML={{ __html: url }}
-              onClick={() => browser.tabs.update(this.props.id, { active: true })}
+              dangerouslySetInnerHTML={{__html: url}}
+              onClick={() => browser.tabs.update(props.id, {active: true})}
             />
             <ul className=" tab-actions" role="group" aria-label="options">
               {actionButtons}
@@ -140,9 +117,9 @@ class Tab extends PureComponent {
         )}
       </Draggable>
     );
-  }
 }
-const mapStateToProps = function(state) {
+
+const mapStateToProps = function (state) {
   return {
     searchTerm: state.preferences.searchTerm,
   };
