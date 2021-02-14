125938f52cbbe0b61f5dc6a10e8bf64b0c4fa09b
Amir Hameed <picwellwisher12pk@gmail.com>
2021-02-14 06:00:26 +0500

    TabsGroup.js refactored to functional component

--- src/scripts/components/Accordion/TabsGroup/TabsGroup.js
+++ src/scripts/components/Accordion/TabsGroup/TabsGroup.js
@@ -1,53 +1,22 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { Droppable } from 'react-beautiful-dnd';
-// import Tab from './Tab/index';
-import { log } from '../../general';
+import React from 'react';
+import {Droppable} from 'react-beautiful-dnd';
 
-export default class TabsGroup extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTabs: [],
-    };
-    this.render = this.render.bind(this);
-  }
+const TabsGroup = () => {
+  return (
+    <Droppable droppableId="droppable" id={'droppable'}>
+      {provided => (
+        <ul
+          className="tab tabs-list sortable selectable"
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          id={'droppableUL'}
+        >
+          {this.props.children}
+          {provided.placeholder}
+        </ul>
+      )}
+    </Droppable>
+  );
+};
+export default TabsGroup;
 
-  componentDidMount() {
-    // console.log("tabsgroup mounted:",this.state.tabs);
-  }
-
-  // return (
-  //   <TransitionGroup component="ul" className="tab tabs-list sortable selectable">
-  //     {this.props.children}
-  //   </TransitionGroup>
-  // );
-
-  render() {
-    // console.log("tabgroup this props children",this.state.tabs,this.props.children);
-    // const animated = `<TransitionGroup component="ul" className="tab tabs-list sortable selectable">
-    //           ${this.props.children}
-    //         </TransitionGroup>`;
-    // const { innerRef } = this.props;
-    // const staticList = `<ul className="tab tabs-list sortable selectable" ref=${provided.innerRef}> ${this.props.children}</ul>`;
-    return (
-      <Droppable droppableId="droppable" id={'droppable'}>
-        {provided => (
-          <ul
-            className="tab tabs-list sortable selectable"
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            id={'droppableUL'}
-          >
-            {this.props.children}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
-    );
-  }
-}
-// TabsGroup.propTypes = {
-//   preferences: PropTypes.object,
-//   tabs: PropTypes.array,
-// };
