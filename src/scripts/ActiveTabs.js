88aa16dcfeb4ff2a1be119e515a21d74410789e7
Amir Hameed <picwellwisher12pk@gmail.com>
2021-04-26 04:24:23 +0500

    Stripped many tab properties to render on ui, to save memory. Trying memo and callback on different location to improve performance. No visible results yet.

--- src/scripts/ActiveTabs.js
+++ src/scripts/ActiveTabs.js
@@ -1,120 +1,162 @@
-'use strict';
+"use strict";
 //Scripts and Modules
-import CustomScroll from 'react-custom-scroll';
-import {useCallback} from 'react';
-import {connect} from 'react-redux';
-import 'react-devtools';
-import {DndProvider} from 'react-dnd';
-import {HTML5Backend} from 'react-dnd-html5-backend'
+import store from "./store";
+import React, {Profiler, useCallback, useEffect, useMemo, useState,} from "react";
+import {useSelector} from "react-redux";
+import {updateActiveTabs} from "./tabSlice";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import {asyncFilterTabs, getMetrics} from "./components/general";
+import {getTabs} from "./components/browserActions";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 //JS libraries
-import '../images/logo.png';
-import '../images/dev-logo.png';
+import "../images/logo.png";
+import "../images/dev-logo.png";
 // React Components
-import ACTIONS from './action';
-
-import Header from './components/Header/Header';
-import Tabsgroup from './components/Accordion/TabsGroup/index';
-import Tab from './components/Accordion/TabsGroup/Tab/index';
+import Header from "./components/Header/Header";
+import Search from "./components/Header/Search/Search";
+import Tab from "./components/Accordion/TabsGroup/Tab/index";
 //Styles
-import '../styles/eg.scss';
+import "../styles/eg.scss";
+import "react-custom-scroll/dist/customScroll.css";
 
-require('react-custom-scroll/dist/customScroll.css');
+const browser = require("webextension-polyfill");
 
-let browser = require('webextension-polyfill');
-
-// import ErrorBoundary from './ErrorBoundary';
-
-const ActiveTabs = (props) => {
-
-  const filterTabs = () => {
-    if (props.preferences.searchTerm === '') return props.tabs;
-    return props.tabs.filter(tab => {
-      if (props.preferences.search.regex) {
-        try {
-          let regex = new RegExp(props.preferences.searchTerm, props.preferences.search.ignoreCase ? 'i' : '');
-          if (props.preferences.search.searchIn[0]) {
-            if (regex.test(tab.title)) return true;
-          }
-          if (props.preferences.search.searchIn[1]) {
-            if (regex.test(tab.url)) return true;
-          }
-        } catch (error) {
-          console.log("search error:", error);
-        }
-
-      } else {
-        if (props.preferences.search.searchIn[0]) {
-          return tab.title.includes(props.preferences.searchTerm);
-        }
-        if (props.preferences.search.searchIn[1]) {
-          return tab.url.includes(props.preferences.searchTerm);
-        }
-      }
-    });
-  }
-
-  const moveTab = useCallback((itemId, dragIndex, index) => {
-    browser.tabs.move(itemId, {index});
-  }, [props.tabs]);
-
-  const tabTemplate = (tab) => {
-    let checked = false;
-    //If no tab is selected, add current tab to selectedTabs
-    if (props.selectedTabs.length === 0) checked = props.selectedTabs.includes(tab.id);
-    return (
-      <Tab
-        key={tab.id}
-        index={tab.index}
-        {...tab}
-        activeTab={true}
-        checked={checked}
-        // closeTab={closeTab}
-        // togglePin={togglePin}
-        // toggleMute={toggleMute}
-        moveTab={moveTab}
-        // updateSelectedTabs={updateSelectedTabs}
-      />
+export function updateTabs(getTabs, store) {
+  getTabs().then((tabs) => {
+    store.dispatch(
+      updateActiveTabs(
+        tabs.map((tab) => {
+          const {
+            audible,
+            discarded,
+            favIconUrl,
+            id,
+            index,
+            mutedInfo,
+            pinned,
+            status,
+            title,
+            url,
+            windowId,
+            groupId,
+          } = tab;
+          return {
+            audible,
+            discarded,
+            favIconUrl,
+            id,
+            index,
+            mutedInfo,
+            pinned,
+            status,
+            title,
+            url,
+            groupId,
+            windowId,
+          };
+        })
+      )
     );
-  }
-
-
-
-
-    return [
-      <Header
-        key={'header'}
-        tabs={props.tabs}
-        preferences={props.preferences}
-        // searchInTabs={searchInTabs}
-        // processSelectedTabs={processSelectedTabs}
-      />,
-      <CustomScroll heightRelativeToParent="100%">
-        <div className="tabs-list-container" key={'activetablist'}>
-          <DndProvider backend={HTML5Backend}>
-            <Tabsgroup preferences={props.preferences} id={'tabsgroup'}>
-              {filterTabs().map(tab => tabTemplate(tab), this)}
-            </Tabsgroup>
-          </DndProvider>
-        </div>
-        ,
-      </CustomScroll>,
-    ];
-
+  });
 }
 
-const mapStateToProps = function(state) {
-  return {
-    tabs: state.tabs,
-    preferences: state.preferences,
-    selectedTabs: state.preferences.selectedTabs,
-  };
+browser.tabs.onRemoved.addListener(() => updateTabs(getTabs, store));
+browser.tabs.onDetached.addListener(() => updateTabs(getTabs, store));
+browser.tabs.onCreated.addListener(() => updateTabs(getTabs, store));
+browser.tabs.onAttached.addListener(() => updateTabs(getTabs, store));
+browser.tabs.onUpdated.addListener(() => updateTabs(getTabs, store));
+browser.tabs.onMoved.addListener(() => updateTabs(getTabs, store));
+updateTabs(getTabs, store);
+
+const TabWindowWrapper = ({tabs, loading, setLoading}) => {
+  const selectedTabs = useSelector((state) => state.tabs.selectedTabs);
+
+  const tabTemplate = useMemo(
+    () => (tab, selectedTabs, moveTab) => {
+      let checked = false;
+      //If no tab is selected, add current tab to selectedTabs
+      if (selectedTabs?.length === 0) checked = selectedTabs.includes(tab.id);
+      return (
+        <Tab
+          {...tab}
+          key={tab.id}
+          index={tab.index}
+          activeTab={true}
+          checked={checked}
+          moveTab={moveTab}
+        />
+      );
+    },
+    []
+  );
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  const moveTab = useCallback(
+    (itemId, dragIndex, index) => {
+      browser.tabs.move(itemId, {index});
+    },
+    [tabs]
+  );
+  const tabsMemo = useMemo(() => {
+    return tabs.map((tab) => tabTemplate(tab, selectedTabs, moveTab));
+  }, [tabs]);
+  return !loading ? (
+    <div className="tabs-list-container">
+      <DndProvider backend={HTML5Backend}>
+        <ul className="tab tabs-list sortable selectable" id={"droppableUL"}>
+          {tabs.length !== undefined && tabsMemo}
+        </ul>
+      </DndProvider>
+    </div>
+  ) : (
+    <ClimbingBoxLoader
+      color={"#12a3a9"}
+      css={{display: "block", margin: "80px auto"}}
+      loading={true}
+      size={20}
+    />
+  );
 };
-const mapDispatchToProps = dispatch => ({
-  reorderTabs: tabs => dispatch(ACTIONS.reorderTabs(tabs)),
-  deleteItem: id => dispatch(ACTIONS.deleteItem(id)),
-  searchInTabs: searchTerm => dispatch(ACTIONS.searchInTabs(searchTerm)),
-  updateActiveTabs: () => dispatch(ACTIONS.updateActiveTabs()),
-  updateSelectedTabsAction: selectedTabs => dispatch(ACTIONS.updateSelectedTabsAction(selectedTabs)),
-});
-export default connect(mapStateToProps, mapDispatchToProps)(ActiveTabs);
+
+const ActiveTabs = () => {
+  const [loading, setLoading] = useState(true);
+  const tabs = useSelector((state) => state.tabs.tabs);
+  const searchPref = useSelector((state) => state.config.preferences.search);
+  const searchTerm = useSelector((state) => state.search.searchTerm);
+  const [filteredTabs, setFilteredTabs] = useState(tabs);
+
+  const findFilteredTabs = useMemo(
+    () => async (searchTerm, searchPref, tabs, setLoading) => {
+      let tempTabs = await asyncFilterTabs(searchTerm, searchPref, tabs);
+      setFilteredTabs(tempTabs);
+      setLoading(false);
+    },
+    [searchTerm, searchPref, tabs]
+  );
+  useEffect(() => {
+    if (searchTerm === "") {
+      setFilteredTabs(tabs);
+    } else {
+      setLoading(true);
+      findFilteredTabs(searchTerm, searchPref, tabs, setLoading);
+    }
+  }, [searchTerm, tabs]);
+  return (
+    <Profiler id={"app"} onRender={getMetrics}>
+      <Header>
+        <Search/>
+      </Header>
+      <TabWindowWrapper
+        tabs={filteredTabs}
+        loading={loading}
+        setLoading={setLoading}
+      />
+    </Profiler>
+  );
+};
+
+export default ActiveTabs;
