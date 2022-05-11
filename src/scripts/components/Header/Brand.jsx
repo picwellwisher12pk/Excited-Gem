d49e19c65d91700a70baeeb1c44fb817c917f2f5
Amir Hameed <picwellwisher12pk@gmail.com>
2022-05-11 22:47:32 +0500

    logo is now a direct url to image file so using it directly instead of logo.default

--- src/scripts/components/Header/Brand.jsx
+++ src/scripts/components/Header/Brand.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Brand = (logo) => {
-  return <div className="navbar-brand ">
-    <a href="#" className="pull-left logo" style={{marginTop: '10px'}}>
-      <img src={logo.default} alt="" style={{height: '40px', width: 'auto'}}/>
-    </a>
-  </div>
+  console.log(logo);
+  return (
+    <div className="navbar-brand ">
+      <a href="#" className="pull-left logo" style={{ marginTop: "10px" }}>
+        <img src={logo} alt="" style={{ height: "40px", width: "auto" }} />
+      </a>
+    </div>
+  );
 };
 export default Brand;
