9089ebff9dd27fcd518ea5f1ce4c2af15ee11218
Amir Hameed <picwellwisher12pk@gmail.com>
2022-05-11 22:41:45 +0500

    Using manifest 2 for now untile version 3 is fixed for CSP rules fixed CSP rules added csp rules for img,svg, inline styling and scripting separeted code for mv2 and mv3

--- src/manifest.js
+++ src/manifest.js
@@ -2,17 +2,17 @@ import fs from "fs-extra";
 // import { Manifest } from "webextension-polyfill";
 // import type PkgType from "../package.json";
 import { isDev, port, r } from "../scripts/utils";
-
+const manifestVersion = 2;
 export async function getManifest() {
   const pkg = await fs.readJSON(r("package.json"));
   // update this file to update this manifest.json
   // can also be conditional based on your need
   const manifest = {
-    manifest_version: 3,
+    manifest_version: manifestVersion,
     name: pkg.displayName || pkg.name,
     version: pkg.version,
     description: pkg.description,
-    action: {
+    browser_action: {
       default_icon: {
         19: "assets/icon.svg",
         38: "assets/icon.svg",
@@ -25,8 +25,7 @@ export async function getManifest() {
     //   chrome_style: false,
     // },
     background: {
-      service_worker: "./background.js",
-      type: "module",
+      scripts: ["./browser-polyfill.js", "./background.js"],
     },
     icons: {
       16: "assets/icon.svg",
@@ -53,19 +52,24 @@ export async function getManifest() {
     // ],
     // web_accessible_resources: ["dist/contentScripts/style.css"],
   };
-
+  const policyString = `default-src 'self' http://localhost:${port};script-src 'self' 'unsafe-eval' http://localhost:${port};connect-src 'self' ws://localhost:${port}; style-src 'self' 'unsafe-inline' http://localhost:${port}; img-src * 'self' data:  ;`;
   if (isDev) {
-    // for content script, as browsers will cache them for each reload,
-    // we use a background script to always inject the latest version
-    // see src/background/contentScriptHMR.ts
-    delete manifest.content_scripts;
-    manifest.permissions?.push("webNavigation");
+    if (manifestVersion === 3) {
+      // for content script, as browsers will cache them for each reload,
+      // we use a background script to always inject the latest version
+      // see src/background/contentScriptHMR.ts
+      delete manifest.content_scripts;
+      manifest.permissions?.push("webNavigation");
 
-    manifest.sandbox = { pages: ["popup.html"] };
-    manifest.content_security_policy = {
-      extension_pages: `default-src 'self' http://localhost:${port};script-src http://localhost:${port} 'self';`,
-      sandbox: `sandbox allow-scripts; script-src 'self' http://localhost:${port};script-src-elem 'self' http://localhost:${port}`,
-    };
+      manifest.sandbox = { pages: ["popup.html"] };
+      manifest.content_security_policy = {
+        // extension_pages: policyString,
+        sandbox: `sandbox allow-scripts; script-src 'self';script-src-elem 'self' http://localhost:${port}`,
+      };
+    }
+    if (manifestVersion === 2) {
+      manifest.content_security_policy = policyString;
+    }
   }
 
   return manifest;
