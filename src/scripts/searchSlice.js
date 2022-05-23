6a44ed66fdc2935c217b7c1f987fce0b6ffaa4a8
Amir Hameed <picwellwisher12pk@gmail.com>
2022-05-24 03:40:29 +0500

    udpated search and tabs slice for immer

--- src/scripts/searchSlice.js
+++ src/scripts/searchSlice.js
@@ -12,37 +12,20 @@ export const searchSlice = createSlice({
     regex,
   },
   reducers: {
-    // Redux Toolkit allows us to write "mutating" logic in reducers. It
-    // doesn't actually mutate the state because it uses the Immer library,
-    // which detects changes to a "draft state" and produces a brand new
-    // immutable state based off those changes
     updateSearchTerm: (state, action) => {
-      let newState = { ...state };
-      // action.payload !== ""
-      //   ? (newState.preferences.search.empty = false)
-      //   : (newState.preferences.search.empty = true);
-      newState.searchTerm = action.payload;
-      return newState;
+      state.searchTerm = action.payload;
     },
     toggleRegex: (state) => {
-      const newState = { ...state };
-      newState.regex = !state.regex;
-      return newState;
+      state.regex = !state.regex;
     },
     toggleSearchIn: (state, action) => {
-      const newState = { ...state };
-      newState.searchIn = { ...action.payload };
-      return newState;
+      state.searchIn = { ...action.payload };
     },
     toggleAudible: (state) => {
-      let newState = { ...state };
-      newState.audibleSearch = !state.audibleSearch;
-      return newState;
+      state.audibleSearch = !state.audibleSearch;
     },
     togglePinned: (state) => {
-      let newState = { ...state };
-      newState.pinnedSearch = !state.pinnedSearch;
-      return newState;
+      state.pinnedSearch = !state.pinnedSearch;
     },
   },
 });
