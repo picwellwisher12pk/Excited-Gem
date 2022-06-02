b7dde7714d39fd52a4cc0719362c9019c74e114d
Amir Hameed <picwellwisher12pk@gmail.com>
2022-06-03 04:01:58 +0500

    added more use cases for maifest 2 and 3 Also added sh256 in csp

--- src/manifest.js
+++ src/manifest.js
@@ -12,21 +12,12 @@ export async function getManifest() {
     name: pkg.displayName || pkg.name,
     version: pkg.version,
     description: pkg.description,
-    browser_action: {
-      default_icon: {
-        19: "assets/icon.svg",
-        38: "assets/icon.svg",
-      },
-      default_title: "Excited Gem",
-    },
     // options_ui: {
     //   page: "./dist/options/index.html",
     //   open_in_tab: true,
     //   chrome_style: false,
     // },
-    background: {
-      scripts: ["./browser-polyfill.js", "./background.js"],
-    },
+
     icons: {
       16: "assets/icon.svg",
       48: "assets/icon.svg",
@@ -52,7 +43,12 @@ export async function getManifest() {
     // ],
     // web_accessible_resources: ["dist/contentScripts/style.css"],
   };
-  const policyString = `default-src 'self' http://localhost:${port};script-src 'self' 'unsafe-eval' http://localhost:${port};connect-src 'self' ws://localhost:${port}; style-src 'self' 'unsafe-inline' http://localhost:${port}; img-src * 'self' data:  ;`;
+  const SHA256 =
+    manifestVersion === 2
+      ? "sha256-9402wHbGtjqsZ8WISJUnqSRxLJpuyt9R0E8g410kSr4="
+      : "";
+  const unsafeEval = manifestVersion === 2 ? "unsafe-eval" : "";
+  const policyString = `default-src 'self' http://localhost:${port};script-src 'self' '${SHA256}'  http://localhost:${port} ;connect-src 'self' ws://localhost:${port}; style-src 'self' 'unsafe-inline' http://localhost:${port}; img-src * 'self' data:  ;`;
   if (isDev) {
     if (manifestVersion === 3) {
       // for content script, as browsers will cache them for each reload,
@@ -60,14 +56,34 @@ export async function getManifest() {
       // see src/background/contentScriptHMR.ts
       delete manifest.content_scripts;
       manifest.permissions?.push("webNavigation");
-
-      manifest.sandbox = { pages: ["popup.html"] };
+      manifest.background = {
+        service_worker: "background.js",
+        type: "module",
+      };
+      // manifest.sandbox = { pages: ["popup.html"] };
       manifest.content_security_policy = {
-        // extension_pages: policyString,
-        sandbox: `sandbox allow-scripts; script-src 'self';script-src-elem 'self' http://localhost:${port}`,
+        extension_pages: policyString,
+        // sandbox: `sandbox allow-scripts; script-src 'self';script-src-elem 'self' http://localhost:${port}`,
+      };
+      manifest.action = {
+        default_icon: {
+          19: "assets/icon.svg",
+          38: "assets/icon.svg",
+        },
+        default_title: "Excited Gem",
       };
     }
     if (manifestVersion === 2) {
+      manifest.browser_action = {
+        default_icon: {
+          19: "assets/icon.svg",
+          38: "assets/icon.svg",
+        },
+        default_title: "Excited Gem",
+      };
+      manifest.background = {
+        scripts: ["./browser-polyfill.js", "./background.js"],
+      };
       manifest.content_security_policy = policyString;
     }
   }
