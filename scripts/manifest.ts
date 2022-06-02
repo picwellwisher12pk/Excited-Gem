5fc88364a9b0381a6f91f0c65879d0b2d8f92c04
Amir Hameed <picwellwisher12pk@gmail.com>
2022-06-03 03:57:57 +0500

    added timestamp while logging while it create manifest

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -1,10 +1,23 @@
-import fs from 'fs-extra'
-import { getManifest } from '../src/manifest'
-import { r, log } from './utils'
+import fs from "fs-extra";
+import { getManifest } from "../src/manifest";
+import { r, log } from "./utils";
 
 export async function writeManifest() {
-  await fs.writeJSON(r('extension/manifest.json'), await getManifest(), { spaces: 2 })
-  log('PRE', 'write manifest.json')
+  let time = new Date();
+
+  await fs.writeJSON(r("extension/manifest.json"), await getManifest(), {
+    spaces: 2,
+  });
+  log(
+    "PRE",
+    "write manifest.json " +
+      time.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+      })
+  );
 }
 
-writeManifest()
+writeManifest();
