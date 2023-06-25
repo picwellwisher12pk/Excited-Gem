40b9746f21fef31ec55d171b3cf5c87215cd5728
Amir Hameed <picwellwisher12pk@gmail.com>
2023-06-26 00:06:31 +0500

    Selector Working with current window as default

--- src/scripts/components/WindowSelector.jsx
+++ src/scripts/components/WindowSelector.jsx
@@ -1,90 +1,136 @@
-import { Badge, Select } from "antd";
-const { Option } = Select;
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { Select } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { updateSelectedWindow } from "../tabSlice";
+import { getAllWindows, getCurrentWindow } from '../general'
+import { updateSelectedWindow } from '../tabSlice'
 
-export default function WindowSelector({ allWindows, currentWindow }) {
-  const dispatch = useDispatch();
-  const [dropdownVisible, setDropDown] = useState(false);
-  const { selectedWindow } = useSelector((state) => state.tabs);
+const { Option } = Select
 
-  const setWindow = (window) => {
-    dispatch(updateSelectedWindow(window));
-    setDropDown(!dropdownVisible);
-  };
+export default function WindowSelector() {
+  const dispatch = useDispatch()
+  // const [dropdownVisible, setDropDown] = useState(false)
+  const { selectedWindow } = useSelector((state) => state.tabs) //'current'
+  const [allWindows, setAllWindows] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [currentWindow, setCurrentWindow] = useState({})
+  const [currentWindowOption, setCurrentWindowOption] = useState({}) //['current'
 
-  const totalTabCount = allWindows.reduce(function (total, window) {
-    return total + window.tabs.length;
-  }, 0);
-  return (
-    <Select
-      style={{ width: 200 }}
-      defaultValue={currentWindow.id}
-      className="!border-0 shadow-md !rounded-[2px] !bg-gradient-to-b !from-white !to-slate-200"
-      onSelect={setWindow}
-    >
-      <Option className="list-group-item" value="all">
-        <div className="flex justify-between">
+  async function getWindows() {
+    const allWindows = await getAllWindows()
+    const currentWindow = await getCurrentWindow()
+    setAllWindows(allWindows)
+    setCurrentWindow(currentWindow)
+    setCurrentWindowOption(optionCreator(currentWindow))
+    setLoading(false)
+  }
+  function optionCreator(window) {
+    return (
+      <Option
+        key={window.id}
+        value={window.id}
+        label={window.id}
+        onClick={() =>
+          setWindow(currentWindow.id === window.id ? 'current' : window.id)
+        }>
+        <span
+          className={`flex justify-between align-items-center btn-link`}
+          onClick={() => {
+            setWindow(window.id)
+          }}>
           <div>
             <span
               className={
-                "inline-block w-2 h-2 rounded-full mb-[1px] mr-2 " +
-                (currentWindow.id === window.id && "bg-green-500")
-              }
-            ></span>
-            <span>All Windows</span>
+                'inline-block w-2 h-2 rounded-full mb-[1px] mr-2 ' +
+                (currentWindow.id === window.id && 'bg-green-500')
+              }></span>
+            <span>
+              Window
+              {currentWindow.id === window.id && (
+                <small className="text-gray-400"> (current)</small>
+              )}
+              <span className="sr-only">(current)</span>
+            </span>
           </div>
           <small
             className={
-              totalTabCount > 50 ? "!text-orange-600" : "!text-lime-700"
-            }
-          >
-            {totalTabCount} tab(s)
+              window.tabs.length > 50 ? '!text-orange-600' : '!text-lime-700'
+            }>
+            {window.tabs.length} tab{window.tabs.length > 1 && 's'}
           </small>
-        </div>
+        </span>
       </Option>
-      {allWindows.map((window) => (
-        <Option
-          className="list-group-item"
-          value={window.id}
-          onClick={() =>
-            setWindow(currentWindow.id === window.id ? "current" : window.id)
-          }
-          key={window.id}
-        >
+    )
+  }
+  useEffect(() => {
+    getWindows()
+  }, [])
+  const setWindow = (window) => {
+    dispatch(updateSelectedWindow(window))
+  }
+
+  const totalTabCount = allWindows.reduce(function (total, window) {
+    return total + window.tabs.length
+  }, 0)
+  const optionAll = (
+    <Option value="all" label="all" key="all">
+      <div className="flex justify-between">
+        <div>
           <span
-            className={`flex justify-between align-items-center btn-link`}
-            onClick={() => {
-              setWindow(window.id);
-            }}
-          >
+            className={
+              'inline-block w-2 h-2 rounded-full mb-[1px] mr-2 ' +
+              (currentWindow.id === window.id && 'bg-green-500')
+            }></span>
+          <span>All Windows</span>
+        </div>
+        <small
+          className={
+            totalTabCount > 50 ? '!text-orange-600' : '!text-green-500'
+          }>
+          {totalTabCount} tab{totalTabCount > 1 && 's'}
+        </small>
+      </div>
+    </Option>
+  )
+
+  const options = allWindows.map(optionCreator)
+  return (
+    <Select
+      loading={loading}
+      style={{ width: 200 }}
+      size={'small'}
+      value={{
+        label: (
+          <span className={`flex justify-between align-items-center btn-link`}>
             <div>
               <span
                 className={
-                  "inline-block w-2 h-2 rounded-full mb-[1px] mr-2 " +
-                  (currentWindow.id === window.id && "bg-green-500")
-                }
-              ></span>
+                  'inline-block w-2 h-2 rounded-full mb-[1px] mr-2 ' +
+                  (currentWindow.id === window.id && 'bg-green-500')
+                }></span>
               <span>
                 Window
                 {currentWindow.id === window.id && (
                   <small className="text-gray-400"> (current)</small>
                 )}
-                <span className="sr-only">(current)</span>
               </span>
             </div>
             <small
               className={
-                window.tabs.length > 50 ? "!text-orange-600" : "!text-lime-700"
-              }
-            >
-              {window.tabs.length} tab(s)
+                currentWindow?.tabs?.length > 50
+                  ? '!text-orange-600'
+                  : '!text-lime-700'
+              }>
+              {currentWindow?.tabs?.length} tab
+              {currentWindow?.tabs?.length > 1 && 's'}
             </small>
           </span>
-        </Option>
-      ))}
+        ),
+        value: currentWindow.id
+      }}
+      className="!border-0 shadow-md !rounded-[2px] !bg-gradient-to-b !from-white !to-slate-200"
+      onChange={setWindow}>
+      {[optionAll, ...options]}
     </Select>
-  );
+  )
 }
