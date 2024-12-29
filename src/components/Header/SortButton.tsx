1e0d690f4e0f13e17cfd73c4f644da941a756c68
Amir Hameed <picwellwisher12pk@gmail.com>
2024-12-29 05:56:24 +0500

    Sort dropdown UI update and refactor

--- src/components/Header/SortButton.tsx
+++ src/components/Header/SortButton.tsx
@@ -1,33 +1,32 @@
-import DownOutlined from '@ant-design/icons'
+import {CaretDownFilled} from '@ant-design/icons'
+import type {MenuProps} from 'antd';
 import {Dropdown} from 'antd'
-import {ReactElement, useCallback} from 'react'
 //@ts-ignore
-import SortIcon from 'react:/src/icons/sort.svg'
-
 import {sortTabs} from '~/scripts/general'
 import Btn from '../Btn'
 
 
 const SortButton = ({tabs}) => {
-  const handleSortByTitle = useCallback(() => sortTabs('title', tabs), [tabs])
-  const handleSortByURL = useCallback(() => sortTabs('url', tabs), [tabs])
-  const items: ({ key: string; label: ReactElement })[] = [
+
+  const onClick: MenuProps['onClick'] = ({key}) => {
+    sortTabs(key, tabs)
+  };
+  const items: ({ key: string; label: string })[] = [
     {
       key: 'title',
-      label: <a onClick={handleSortByTitle}>Title</a>
+      label: "By Title"
     },
     {
       key: 'url',
-      label: <a onClick={handleSortByURL}>URL</a>
+      label: "By URL"
     }
   ]
 
   return (
-    <Dropdown menu={{items}} className="mr-3" trigger={['click']}>
+    <Dropdown menu={{items, onClick}} className="mr-3" trigger={['click']}>
       <Btn>
-        <SortIcon className="!fill-slate-700" style={{height: 12}}/>
-        <span>Sort</span>
-        <DownOutlined className="text-zinc-500"/>
+        <span>Sort Tabs</span>
+        <CaretDownFilled/>
       </Btn>
     </Dropdown>
   )
